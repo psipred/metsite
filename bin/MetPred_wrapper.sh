47dc903558e8372e297b27cb40da198716a603a0
Daneil Buchan <clone@myself.com>
2017-02-20 16:35:22 +0000

    added wrapper to run MetPred

---
+++ bin/MetPred_wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# MetPred_wrapper.sh /opt/Code/metsite CU 1 1IAR.pdb.DATA 1IAR_MetPred.pdb A
+#
+path=$1
+metal=$2
+fpr_choice=$3
+seedsitedata=$4
+pdb=$5
+chain=$6
+output=`java -cp $path/src/org/ucl/conf/:$path/src returnNetCut $metal $fpr_choice`
+value=${output##*' '}
+lc=${metal,,}
+echo $lc
+`$path/bin/MetPred $seedsitedata $lc $value $pdb $chain $path/data/`
